121a44f9546b0b4430a1f9eacf4a2dac15ec0932
Mael Guillossou <mael.guillossou@gmail.com>
2023-06-29 23:46:09 +0200

    bugfix(db): dataabse type

--- backend/bbe2/models/update.py
+++ backend/bbe2/models/update.py
@@ -1,20 +1,23 @@
 import enum
+
 from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String, func
 from sqlalchemy.orm import relationship
 
 from bbe2.database import Base
+
 
 class UpdateAction(enum.Enum):
     CREATE = "CREATE"
     UPDATE = "UPDATE"
     DELETE = "DELETE"
 
+
 class Update(Base):
     __tablename__ = "updates"
 
     id = Column(Integer, primary_key=True)
     updated_at = Column(DateTime, server_default=func.now())
-    updated_by_id = Column(Integer, ForeignKey("profiles.id"))
+    updated_by_id = Column(String, ForeignKey("profiles.id"))
     updated_by = relationship("Profile")
     action = Column(Enum(UpdateAction))
     entity_type = Column(String)
