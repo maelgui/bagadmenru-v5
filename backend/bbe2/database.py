8dd5e7690dc555c19290b4cfa4f59d803adb681d
Mael Guillossou <mael.guillossou@gmail.com>
2023-06-29 23:35:39 +0200

    bigfix(db): remove incompatibe engine arg

--- backend/bbe2/database.py
+++ backend/bbe2/database.py
@@ -7,9 +7,7 @@ from bbe2.config import settings
 SQLALCHEMY_DATABASE_URL = settings.database_url
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
