93c63c1f9d8c880d198644e6cfd2df0fcf729775
Mael G <1678635+maelgui@users.noreply.github.com>
2023-10-24 18:02:55 +0200

    Update oidc.ts

--- frontend/app/src/config/oidc.ts
+++ frontend/app/src/config/oidc.ts
@@ -10,4 +10,5 @@ export default {
   silent_redirect_uri: `${window.location.origin}/authentication/silent-callback`, // Optional activate silent-signin that use cookies between OIDC server and client javascript to restore the session
   scope: 'openid profile email offline_access',
   authority: import.meta.env.VITE_OIDC_PROVIDER_URL,
+  storage: localStorage
 } as OidcConfiguration;
