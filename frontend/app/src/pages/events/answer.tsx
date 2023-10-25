5f75ae2f4d7481508e3f2043bace488ef5809eb6
Mael Guillossou <mael.guillossou@gmail.com>
2023-10-25 23:52:42 +0200

    fix build

--- frontend/app/src/pages/events/answer.tsx
+++ frontend/app/src/pages/events/answer.tsx
@@ -1,37 +1,78 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { useOidcIdToken } from '@axa-fr/react-oidc';
+import { faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { ResponseCreate } from 'bagad-client';
 import Button from '../../components/button';
 import Container from '../../components/container';
-import { useApiClient } from '../../config/client';
+import { queryClient, useApiClient } from '../../config/client';
 import EventListItem from './components/event';
 
 export default function AnswerLinkPage() {
+  const { idTokenPayload } = useOidcIdToken();
+
   const { eventsApi } = useApiClient();
+  const eventId = 1;
 
-  // const { data } = useQuery({
-  //   queryKey: ['events', 1],
-  //   queryFn: () => eventsApi.getEventApiV1EventsEventIdGet({ eventId: 1 }),
-  // });
+  const { data: event } = useQuery({
+    queryKey: ['events', eventId],
+    queryFn: () => eventsApi.getEventApiV1EventsEventIdGet({ eventId }),
+  });
 
-  // if (!data) {
-  //   return null;
-  // }
+  const { data: response } = useQuery({
+    queryKey: ['responses', 'me'],
+    queryFn: () => eventsApi.listResponsesApiV1ResponsesGet({ userId: idTokenPayload.sub }),
+    select: (data) => data.filter((r) => r.eventId === eventId).at(0),
+  });
 
-  const data = null;
+  const mutation = useMutation({
+    mutationFn: ({ r }: { r: ResponseCreate }) => {
+      const params = { eventId, responseCreate: r };
+      return eventsApi.createResponseApiV1EventsEventIdResponsesPut(params);
+    },
+    onSettled: () => queryClient.invalidateQueries({ queryKey: ['responses'] }),
+  });
+
+  if (!event) {
+    return null;
+  }
 
   return (
     <Container className="transition-all">
-      {data ? (
+      {event ? (
         <div>
-          <EventListItem event={data} className="border-2 border-pourpre-400 rounded mb-8" />
-          <h4 className="mb-4 font-medium">Serez-vous présent ?</h4>
-          <div className="flex justify-center ">
-            <Button type="button" className="rounded-full">Oui</Button>
-            <Button type="button" className="rounded-full">Non</Button>
-            <Button type="button" className="rounded-full" disabled>Peut-être</Button>
-          </div>
+          <EventListItem event={event} className="border-2 border-pourpre-400 rounded mb-8" />
+          {response ? (
+
+            <div>
+              {response.value ? (
+                <>
+                  <FontAwesomeIcon icon={faCircleCheck} className="text-emerald-300" />
+                  {' '}
+                  Vous serez présent
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon icon={faCircleXmark} className="text-red-300" />
+                  {' '}
+                  Vous ne serez pas présent
+                </>
+              )}
+            </div>
+
+          ) : (
+            <>
+              <h4 className="mb-4 font-medium">Serez-vous présent ?</h4>
+              <div className="flex justify-center ">
+                <Button type="button" className="rounded-full" onClick={() => mutation.mutate({ r: { value: true } })}>Oui</Button>
+                <Button type="button" className="rounded-full" onClick={() => mutation.mutate({ r: { value: false } })}>Non</Button>
+                <Button type="button" className="rounded-full" disabled>Peut-être</Button>
+              </div>
+            </>
+          )}
         </div>
-      )
-        : null}
+      ) : null}
     </Container>
   );
 }
