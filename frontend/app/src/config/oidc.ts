3d77409ab66efa7facb82ac78c3d3b3dcff917d0
Mael Guillossou <mael.guillossou@gmail.com>
2024-03-11 17:06:43 +0100

    add frontend env

--- frontend/app/src/config/oidc.ts
+++ frontend/app/src/config/oidc.ts
@@ -5,7 +5,7 @@
 import { OidcConfiguration } from '@axa-fr/react-oidc';
 
 export default {
-  client_id: 'bbe2-frontend',
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
   redirect_uri: `${window.location.origin}/authentication/callback`,
   silent_redirect_uri: `${window.location.origin}/authentication/silent-callback`, // Optional activate silent-signin that use cookies between OIDC server and client javascript to restore the session
   scope: 'openid profile email offline_access',
--- frontend/app/src/vite-env.d.ts
+++ frontend/app/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_BBE2_API_URL: string
   readonly VITE_OIDC_PROVIDER_URL: string
+  readonly VITE_OIDC_CLIENT_ID: string
 }
 
 interface ImportMeta {
