333d6b7173a128c9701edbe19cc4cd5b2b5e6328
Mael G <1678635+maelgui@users.noreply.github.com>
2024-08-06 09:48:45 +0200

    Update oidc.ts

--- frontend/app/src/config/oidc.ts
+++ frontend/app/src/config/oidc.ts
@@ -7,4 +7,5 @@ export default {
   scope: 'openid profile email offline_access',
   authority: import.meta.env.VITE_OIDC_PROVIDER_URL,
   storage: localStorage,
+  token_request_extras: { audience: 'https://api.beta.bagadmenru.bzh' },
 } as OidcConfiguration;
