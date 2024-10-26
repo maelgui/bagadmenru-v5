300930b05e8a8fc18baf3731c91a97797f9e8a04
Mael Guillossou <mael.guillossou@gmail.com>
2024-10-26 13:15:57 +0200

    use env frontend oidc

--- frontend/app/src/layout/auth-provider.tsx
+++ frontend/app/src/layout/auth-provider.tsx
@@ -3,16 +3,15 @@ import { AuthProvider, AuthProviderProps } from 'react-oidc-context';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 const oidcConfig: AuthProviderProps = {
-  authority: 'http://localhost:3000',
-  client_id: 'bagad-frontend-dev',
-  redirect_uri: 'http://localhost:5173/callback',
+  authority: import.meta.env.VITE_OIDC_PROVIDER_URL,
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
+  redirect_uri: `${window.location.origin}/callback`,
 };
 
 export default function MyAuthProvider() {
   const navigate = useNavigate();
 
   const onSigninCallback = (_user: User | undefined) => {
-    console.log(_user, 'blabla');
     navigate(_user?.url_state || window.location.pathname);
   };
 
