ca341e4acbb5be27d6f4d4340ca8fa154b17c11f
Mael Guillossou <gjm@amazon.fr>
2024-11-25 10:21:01 +0100

    include credentials fetchapi

--- frontend/app/src/config/client.ts
+++ frontend/app/src/config/client.ts
@@ -42,6 +42,7 @@ export const queryClient = new QueryClient({
 export function useApiClient() {
   const conf = new Configuration({
     basePath: env.VITE_BBE2_API_URL,
+    credentials: 'include',
     // headers: { Authorization: `Bearer ${auth.user?.access_token}` },
   });
 
