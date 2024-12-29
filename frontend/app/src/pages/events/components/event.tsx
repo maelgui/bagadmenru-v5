391119bd5102a91d3f81e7746c2c6022dc6959b4
Mael Guillossou <mael.guillossou@gmail.com>
2024-12-29 12:46:21 +0100

    fix

--- frontend/app/src/pages/events/components/event.tsx
+++ frontend/app/src/pages/events/components/event.tsx
@@ -34,7 +34,7 @@ export default function EventListItem({
           <div className="p-1">
             <Badge color={EventCategories[event.category]?.bg ?? 'bg-gray-500'}>{EventCategories[event.category]?.name ?? event.category}</Badge>
           </div>
-          {showResponse ? (
+          {showResponse && event.isInDoodle ? (
             <div className="w-6 h-6 flex items-center justify-center">
               {(() => {
                 if (response === undefined) {
