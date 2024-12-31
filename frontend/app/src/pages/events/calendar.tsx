7f9af1af11343c1516d05924ceea60f837d0e43b
Mael G <1678635+maelgui@users.noreply.github.com>
2024-12-31 09:47:24 +0100

    Update calendar.tsx

--- frontend/app/src/pages/events/calendar.tsx
+++ frontend/app/src/pages/events/calendar.tsx
@@ -33,7 +33,7 @@ export default function CalendarPage() {
   const currentMonth = (new Date(today.getFullYear(), today.getMonth() + monthOffset)).toLocaleString('fr', { month: 'long', year: 'numeric' });
   const [copyButtonLabel, setCopyButtonLabel] = useState('ICS');
   const copy = () => {
-    navigator.clipboard.writeText(`${env.VITE_BBE2_API_URL}/events/export/ics`);
+    navigator.clipboard.writeText(`${env.VITE_BBE2_API_URL}/api/v1/events/export/ics`);
     setCopyButtonLabel('Copié !');
     setTimeout(() => {
       setCopyButtonLabel('ICS');
@@ -70,7 +70,7 @@ export default function CalendarPage() {
             affichées ici peuvent ainsi s&apos;ajouter automatiquement dans votre calendrier !
           </p>
           <p className="pb-2">
-            <Button as={Link} to={`https://www.google.com/calendar/render?cid=${env.VITE_BBE2_API_URL}/events/export/ics`} variant="ghost" icon={faCalendarDay}>Google Agenda</Button>
+            <Button as={Link} to={`https://www.google.com/calendar/render?cid=${env.VITE_BBE2_API_URL}/api/v1/events/export/ics`} variant="ghost" icon={faCalendarDay}>Google Agenda</Button>
             <Button
               variant="ghost"
               icon={faCalendarDay}
