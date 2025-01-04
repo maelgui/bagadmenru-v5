eb5cb35597328827776c6fc2a2f491c7f00f0a58
Mael Guillossou <mael.guillossou@gmail.com>
2025-01-04 17:14:23 +0100

    fix events display

--- frontend/app/src/pages/events/events.tsx
+++ frontend/app/src/pages/events/events.tsx
@@ -38,25 +38,27 @@ export default function EventsManagePage() {
           <div className="overflow-x-auto">
             <ul className="divide-y">
               {events.map((event) => (
-                <li key={event.id} className="gap-4 flex items-center hover:bg-gray-50 relative px-8 py-4">
+                <li key={event.id} className="gap-2 md:flex items-center hover:bg-gray-50 relative px-8 py-4">
                   <div className="flex-1 md:flex items-center">
 
-                    <div>
+                    <div className="mr-4">
                       <Link to={`/events/edit/${event.id}`}>
+                        <Badge className="align-middle" color={EventCategories[event.category]?.bg ?? 'bg-gray-500'}>{EventCategories[event.category]?.name ?? event.category}</Badge>
                         <span className="absolute top-0 bottom-0 left-0 right-0" />
-                        <span className="block whitespace-nowrap">
+                        <span className="block">
                           {event.title}
-                          <Badge className="mx-4 align-middle" color={EventCategories[event.category]?.bg ?? 'bg-gray-500'}>{EventCategories[event.category]?.name ?? event.category}</Badge>
                         </span>
                         <span className="text-sm text-gray-500">{event.description}</span>
                       </Link>
                     </div>
-                    <div className="ml-auto">
+                    <div className="ml-auto whitespace-nowrap">
                       {event.date.toLocaleDateString(undefined, { dateStyle: 'full' })}
                     </div>
                   </div>
-                  <div className="p-8">
-                    <FontAwesomeIcon icon={faChevronRight} className="text-gray-500" />
+                  <div className="px-8 py-2 text-gray-500 text-sm font-semibold">
+                    DÉTAILS
+                    {' '}
+                    <FontAwesomeIcon icon={faChevronRight} className="" />
                   </div>
                 </li>
               ))}
