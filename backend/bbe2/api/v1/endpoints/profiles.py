dde6ccec3f2a42f132b022dc796c13e6c590ed99
Mael G <1678635+maelgui@users.noreply.github.com>
2026-05-03 19:45:02 +0200

    Update profiles.py

--- backend/bbe2/api/v1/endpoints/profiles.py
+++ backend/bbe2/api/v1/endpoints/profiles.py
@@ -13,7 +13,6 @@ from sqlalchemy.types import Integer
 from bbe2 import models, schemas
 from bbe2.crud import CRUDProfile
 from bbe2.dependencies import S3Dep, SenderDep, SessionDep, SettingsDep, get_s3_helper
-from bbe2.utils.templates import EmailData
 from bbe2.schemas.profile import MinimalGroup, Profile
 from bbe2.schemas.utils import (
     GlobalStats,
@@ -30,6 +29,7 @@ from bbe2.utils.auth import (
     Resource,
     get_current_user2,
 )
+from bbe2.utils.templates import EmailData
 
 profiles_router = APIRouter(prefix="/profiles")
 
