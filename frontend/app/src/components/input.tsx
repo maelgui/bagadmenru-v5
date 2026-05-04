79e141e7f118172dcc6ab9d226115c396ce3197f
Mael Guillossou <gjm@amazon.fr>
2026-05-04 16:47:17 +0200

    smaller input border

--- frontend/app/src/components/input.tsx
+++ frontend/app/src/components/input.tsx
@@ -26,7 +26,7 @@ function PrivateInput(
       >
         <input
           ref={ref}
-          className={`block w-full border-gray-200 rounded py-2 px-4 border-2 focus:outline-none focus:bg-white focus:border-pourpre-400 hover:bg-gray-50 invalid:border-red-600 ${error ? 'border-red-500' : ''} disabled:bg-gray-100 disabled:text-gray-600 disabled:cursor-not-allowed ${className ?? ''}`}
+          className={`block w-full border-gray-200 rounded py-2 px-4 border focus:outline-none focus:bg-white focus:border-pourpre-400 focus:ring-1 focus:ring-pourpre-400 hover:bg-gray-50 invalid:border-red-600 ${error ? 'border-red-500' : ''} disabled:bg-gray-100 disabled:text-gray-600 disabled:cursor-not-allowed ${className ?? ''}`}
           aria-invalid={error ? 'true' : 'false'}
           type={currentType}
           // eslint-disable-next-line react/jsx-props-no-spreading
--- frontend/app/src/components/select.tsx
+++ frontend/app/src/components/select.tsx
@@ -14,7 +14,7 @@ function PrivateSelect(
     <>
       <select
         ref={ref}
-        className={`block w-full border-gray-200 rounded py-2 px-4 border-2 focus:outline-none focus:bg-white focus:border-pourpre-400 hover:bg-gray-50 invalid:border-red-600 ${error ? 'border-red-500' : ''} disabled:bg-gray-100 disabled:text-gray-600 disabled:cursor-not-allowed`}
+        className={`block w-full border-gray-200 rounded py-2 px-4 border focus:outline-none focus:bg-white focus:border-pourpre-400 focus:ring-1 focus:ring-pourpre-400 hover:bg-gray-50 invalid:border-red-600 ${error ? 'border-red-500' : ''} disabled:bg-gray-100 disabled:text-gray-600 disabled:cursor-not-allowed`}
         aria-invalid={error ? 'true' : 'false'}
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...rest}
